9293bda6b366850fe1b7cdd53b27780cbb46ce60
Vikas Rawal <vikasrawal@gmail.com>
2018-08-05 18:25:46 +0530

    Added Vikas's personal preferences

---
+++ personal/custom.el
@@ -0,0 +1,10 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-firefox))
+ '(org-export-with-broken-links (quote mark))
+ '(package-selected-packages
+   (quote
+    (ox-gfm csv-mode highlight-parentheses org-gcal org-plus-contrib zop-to-char zenburn-theme yaml-mode which-key volatile-highlights visual-fill-column use-package undo-tree tango-plus-theme swiper-helm smartrep smartparens smart-newline smart-mode-line-powerline-theme ov orgtbl-show-header orgtbl-aggregate org2jekyll org-wc org-variable-pitch org-table-sticky-header org-table-comment org-super-agenda org-sticky-header org-ref org-bullets operate-on-number ob-async move-text magit imenu-anywhere helm-youtube helm-w3m helm-tramp helm-sql-connect helm-spotify-plus helm-spotify helm-rhythmbox helm-recoll helm-projectile helm-ispell helm-google helm-gitlab helm-gitignore helm-github-stars helm-git-grep helm-git-files helm-flyspell helm-flymake helm-flycheck helm-flx helm-emms helm-descbinds helm-c-yasnippet helm-bibtexkey helm-bbdb helm-ag helm-addressbook helm-ack helm-R guru-mode grizzl god-mode gitconfig-mode git-timemachine gist expand-region editorconfig ebib easy-kill discover-my-major diminish diff-hl crux cdlatex browse-kill-ring bibtex-utils bibretrieve bibliothek beacon auctex-latexmk anzu ace-window))))
---
+++ personal/prelude-preference.el
@@ -0,0 +1,4 @@
+;; (setq-clean-whitespace-on-save nil)
+(setq prelude-whitespace nil)
+(setq flx-ido-threshold 1000)
+;; (setq prelude-flyspell nil)
