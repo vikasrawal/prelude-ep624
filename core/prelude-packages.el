bf70886e66f230fc0d877502613aad48e53ef651
Vikas Rawal <vikasrawal@gmail.com>
2018-08-11 12:00:55 +0530

    Fixed prelude-packages.el for orgmode

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -40,7 +40,9 @@
 (if (eq system-type 'windows-nt)
     (add-to-list 'package-archives
                  '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives
+    (add-to-list 'package-archives
+                 '("org" . "http://orgmode.org/elpa/") t)
+    (add-to-list 'package-archives
                '("melpa" . "https://melpa.org/packages/") t))
 
 ;; load the pinned packages
