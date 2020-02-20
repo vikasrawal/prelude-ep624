74bc37d510159432f9c47bb8c018cef5ae29417e
Vikas Rawal <vikasrawal@gmail.com>
2020-02-20 19:32:19 +0530

    Updated custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -8,28 +8,3 @@
  '(package-selected-packages
    (quote
     (ox-gfm csv-mode highlight-parentheses org-gcal org-plus-contrib zop-to-char zenburn-theme yaml-mode which-key volatile-highlights visual-fill-column use-package undo-tree tango-plus-theme swiper-helm smartrep smartparens smart-newline smart-mode-line-powerline-theme ov orgtbl-show-header orgtbl-aggregate org2jekyll org-wc org-variable-pitch org-table-sticky-header org-table-comment org-super-agenda org-sticky-header org-ref org-bullets operate-on-number ob-async move-text magit imenu-anywhere helm-youtube helm-w3m helm-tramp helm-sql-connect helm-spotify-plus helm-spotify helm-rhythmbox helm-recoll helm-projectile helm-ispell helm-google helm-gitlab helm-gitignore helm-github-stars helm-git-grep helm-git-files helm-flyspell helm-flymake helm-flycheck helm-flx helm-emms helm-descbinds helm-c-yasnippet helm-bibtexkey helm-bbdb helm-ag helm-addressbook helm-ack helm-R guru-mode grizzl god-mode gitconfig-mode git-timemachine gist expand-region editorconfig ebib easy-kill discover-my-major diminish diff-hl crux cdlatex browse-kill-ring bibtex-utils bibretrieve bibliothek beacon auctex-latexmk anzu ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Inconsolata" :slant normal :weight normal :height 0.8 :width normal))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro" :height 1.3 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro" :height 1.25))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro" :height 1.2))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro" :height 1.15))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#DCDCCC" :font "Minion Pro"))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(variable-pitch ((t (:family "Minion Pro" :height 140 :weight normal)))))
